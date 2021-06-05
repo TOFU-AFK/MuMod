714b362870c6a915fe3e52b500b63e092bf18051
I-hope1 <a2021QWERTY@126.com>
2021-06-05 20:06:32 +0800

    修改底栏

--- html/mod/bottom/index.js
+++ html/mod/bottom/index.js
@@ -11,4 +11,36 @@ Vue.component('bottom-item', {
 	template: `<span @click="click()"><slot></slot></span>`
 })
 
-new Vue({el: '#bottom'});
+new Vue({
+	el: '#bottom',
+	data: {
+		startY: 0, focus: false,
+		max: Math.ceil($('#bottom #more')[0].children.length / 8) * 32,
+	},
+	methods: {
+		getY() {
+			return event == null ? 0 : event instanceof MouseEvent ? event.clientY : event.touches[0].clientY;
+		},
+		reset() {
+			this.more = $('#bottom #more')[0];
+			this.checkbox = document.getElementById('bottom_more_checkbox')
+			this.startY = this.getY()
+			this.focus = true
+		},
+		move() {
+			this.more.style.height = Math.clamp(this.max + 6, this.startY - this.getY(), 0) + 'px';
+			
+		},
+		moveend() {
+			this.focus = false
+			let h = parseInt(this.more.style.height)
+			if ((h > this.max / 2 && !this.checkbox.checked) || (h < this.max / 2 && this.checkbox.checked)) this.checkbox.click()
+			this.more.style.height = ''
+		}
+	},
+	created() {
+		window.addEventListener('mouseup', () => this.moveend())
+		this.reset()
+		this.more.style.setProperty('--maxh', this.max + 'px')
+	}
+});
