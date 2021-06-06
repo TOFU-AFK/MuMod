b127e4cfe9058b733b830896192012f514b8fe41
TOFU-AFK <72191075+TOFU-AFK@users.noreply.github.com>
2021-06-06 21:09:03 +0800

    Update page.js

--- html/components/left/page.js
+++ html/components/left/page.js
@@ -1,23 +1,10 @@
-Vue.component('left-tab', {
-  template: `<div class="mumod_left_tab"><slot></slot></div>`
-})
-
-Vue.component('left-tab-item', {
+Vue.component('page-top', {
   props: {
-    itemIcon: String,
-    index: Number
+    title: String
   },
-  methods: {
-    click: function(){
-      left.item = this.index;
-      left.title = left.itemArray[this.index].title;
-    }
-  },
-  computed: {
-    getIcon: function() {
-      return '<i class="' + this.itemIcon + ' icon"></i>'
-    }
-  },
-  template: `<div @click='click()' class="mumod_tab_item" v-html='getIcon'>
-      </div>`
-});
+  template: `<div class="mumod_page_top">
+          <div class="mumod_page_top_title">
+            {{ title }}
+          </div>
+        </div>`
+})
