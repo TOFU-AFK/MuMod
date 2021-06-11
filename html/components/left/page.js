74cd62ca880f8f00d1a54e9dc24bebf0c39d956e
TOFU-AFK <72191075+TOFU-AFK@users.noreply.github.com>
2021-06-11 21:08:17 +0800

    Update page.js

--- html/components/left/page.js
+++ html/components/left/page.js
@@ -1,10 +1,14 @@
 Vue.component('page-top', {
   props: {
-    title: String
+    title: String,
+    page: String
   },
-  template: `<div class="mumod_page_top">
+  template: `
+  <div class="mumod_page_top">
           <div class="mumod_page_top_title">
             {{ title }}
+          </div>
+          <div style='padding: 6px' v-html='page'>
           </div>
         </div>`
 })
